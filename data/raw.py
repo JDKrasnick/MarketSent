d63a5960f1e23b8791819c51692317c82ad08b76
JD <jdkrasnick@gmail.com>
2025-10-27 00:25:15 -0400

    Trying out basic PostGres and setting up reddit API

--- data/raw.py
+++ data/raw.py
@@ -1,11 +1,28 @@
 import psycopg2
 
+CLIENT_ID = 'bkdE9Ydi0PcMTIeNPT_pMQ'
+
+
 conn = psycopg2.connect(host="localhost", dbname = "postgres", user = "postgres", password = "ils&s!poJke5", port = "5432")
 
 
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXIST")
+cur.execute("""CREATE TABLE IF NOT EXISTS post(
+    id SERIAL PRIMARY KEY,
+    text TEXT,
+    ticker varchar(10),
+    sentiment varchar(10),
+    score FLOAT,
+    creation DATE
+);
+""")
+
+    
+cur.execute("""SELECT * FROM post where SCORE > 1;""")
+
+print(cur.fetchall())
+
 
 conn.commit()
 cur.close()
