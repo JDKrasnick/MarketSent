df33b8fd2a74175fdf8b7bd564f2bab7e761f21d
JD <jdkrasnick@gmail.com>
2025-12-25 16:04:12 -0500

    Dashboard creation

--- src/api/app.py
+++ src/api/app.py
@@ -85,7 +85,7 @@ def create_app(config: dict = None) -> Flask:
                 "http://127.0.0.1:3000",
                 "http://127.0.0.1:5173",
                 "http://localhost:5173"
-                # TODO Add prod server
+                "https://market-sent.vercel.app/"
             ],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"]
